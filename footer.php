46b5f90a5ae63a53a7f05ce5a64689a8326cb9f0
f7odor <alexyfyodorov@gmail.com>
2017-10-11 21:19:23 +0300

    add Menus

--- footer.php
+++ footer.php
@@ -11,6 +11,6 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 </body>
 </html>
