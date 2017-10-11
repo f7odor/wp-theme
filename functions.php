ca6bb7376c8af71f264cdd4927d2a3f8729f1d12
f7odor <alexyfyodorov@gmail.com>
2017-10-11 23:42:54 +0300

    add Post & img thumbnails

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
 
   // Theme Support
   function wpb_theme_setup(){
+    add_theme_support('post-thumbnails');
+
     //Nav menu
     register_nav_menus(array(
       'primary' => __('Primary Menu')
@@ -11,3 +13,10 @@
   }
 
   add_action('after_setup_theme', 'wpb_theme_setup');
+
+  // Excerpt Length function
+  function set_excerpt_length(){
+    return 45;
+  }
+
+  add_filter('excerpt_length', 'set_excerpt_length');
