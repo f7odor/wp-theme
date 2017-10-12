043502db463a1bf9e587cf1ccb351b34c480cf4b
f7odor <alexyfyodorov@gmail.com>
2017-10-13 00:50:11 +0300

    create Front Page

--- front-page.php
+++ front-page.php
@@ -7,12 +7,10 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <link rel="icon" href="../../favicon.ico">
-
     <title>
       <?php bloginfo('name'); ?> |
       <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
     </title>
-
     <!-- Bootstrap core CSS -->
     <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
@@ -41,8 +39,44 @@
         </nav>
       </div>
     </div>
-    <div class="container">
-      <div class="blog-header">
-        <h1 class="blog-title"><?php bloginfo('name'); ?></h1>
-        <p class="lead blog-description"><?php bloginfo('description'); ?></p>
+<section class="showcase">
+  <div class="container">
+    <h1>Custom Bootstrap WordPress Theme</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod mollitia cum repudiandae officia temporibus saepe unde, odit quae, ducimus perspiciatis! Rerum quam, blanditiis!</p>
+    <a class="btn btn-outline btn-xl js-scroll-trigger">Read More</a>
+    <a href="#download" class="btn btn-outline btn-xl js-scroll-trigger">Start Now for Free!</a>
+  </div>
+</section>
+<section class="boxes">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+          <?php if(is_active_sidebar('box1')) : ?>
+            <?php dynamic_sidebar('box1'); ?>
+          <?php endif; ?>
       </div>
+      <div class="col-md-4">
+          <?php if(is_active_sidebar('box2')) : ?>
+            <?php dynamic_sidebar('box2'); ?>
+          <?php endif; ?>
+      </div>
+      <div class="col-md-4">
+          <?php if(is_active_sidebar('box3')) : ?>
+            <?php dynamic_sidebar('box3'); ?>
+          <?php endif; ?>
+      </div>
+    </div>
+  </div>
+</section>
+<footer class="blog-footer">
+  <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+</footer>
+<?php wp_footer(); ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+</body>
+</html>
