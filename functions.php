0d1d040478a11ad64350f6f8cbfce894849960db
f7odor <alexyfyodorov@gmail.com>
2017-10-13 00:06:42 +0300

    Post Page & Create Gallery

--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@
     register_nav_menus(array(
       'primary' => __('Primary Menu')
     ));
+
+    add_theme_support('post-formats', array('aside', 'gallery'));
   }
 
   add_action('after_setup_theme', 'wpb_theme_setup');
@@ -21,12 +23,11 @@
 
   add_filter('excerpt_length', 'set_excerpt_length');
 
-  // Widget Locations
 
   // Widget Locations
   function wpb_init_widgets($id){
     register_sidebar(array(
-      'name'  => 'Sidebar',
+      'name'  => 'sidebar',
       'id'    => 'sidebar',
       'before_widget' => '<div class="sidebar-module">',
       'after_widget'  => '</div>',
