8cc77908796146cbb236e41be7773af106d25317
f7odor <alexyfyodorov@gmail.com>
2017-10-12 15:18:03 +0300

    add widget function'sidebar' & create Archive page

--- functions.php
+++ functions.php
@@ -22,14 +22,40 @@
   add_filter('excerpt_length', 'set_excerpt_length');
 
   // Widget Locations
+
+  // Widget Locations
   function wpb_init_widgets($id){
     register_sidebar(array(
-      'name' => 'Sidebar',
-      'id' => 'sidebar',
+      'name'  => 'Sidebar',
+      'id'    => 'sidebar',
       'before_widget' => '<div class="sidebar-module">',
-      'arter_widget' => '</div>',
-      'before_title' => '<h4>',
-      'after_title' => '</h4>'
+      'after_widget'  => '</div>',
+      'before_title'  => '<h4>',
+      'after_title'   => '</h4>'
+    ));
+    register_sidebar(array(
+      'name'  => 'Box1',
+      'id'    => 'box1',
+      'before_widget' => '<div class="box">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>'
+    ));
+    register_sidebar(array(
+      'name'  => 'Box2',
+      'id'    => 'box2',
+      'before_widget' => '<div class="box">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>'
+    ));
+    register_sidebar(array(
+      'name'  => 'Box3',
+      'id'    => 'box3',
+      'before_widget' => '<div class="box">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>'
     ));
   }
 
